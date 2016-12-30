082288be3036130703a25953c076458f0afcfcb2
Jared Barnes <jared.j.barnes@gmail.com>
2016-12-30 16:31:25 -0700

    Added the Sprite System.

---
+++ systems/SpriteSystem.ts
@@ -0,0 +1,85 @@
+import Sprite = require("./../components/Sprite");
+import Game = require("./../Game");
+import Entity = require("./../Entity");
+
+const DEPENDENCIES = ["sprite", "image-texture"];
+
+class SpriteSystem {
+    game: Game;
+    entities: Map<string, Entity>;
+
+    constructor() {
+        this.game = null;
+        this.entities = new Map();
+    }
+
+    cacheEntities() {
+        this.game.getEntities().forEach((entity) => {
+            this.entityAdded(entity);
+        });
+    }
+
+    update() {
+
+        this.entities.forEach((entity) => {
+            var sprite = entity.getComponent<Sprite>("sprite");
+            var imageTexture = entity.getComponent("image-texture");
+
+            var index = Math.floor(sprite.index);
+            var newImageTexture = sprite.images[index];
+
+            if (newImageTexture == null) {
+                return;
+            }
+
+            Object.keys(newImageTexture).forEach(function (key) {
+                if (key === "type") {
+                    return;
+                }
+                imageTexture[key] = newImageTexture[key];
+            });
+
+            sprite.index += (sprite.timeScale * 1);
+            sprite.index = sprite.index >= sprite.images.length ? 0 : sprite.index;
+        });
+
+
+    }
+
+    entityAdded(entity: Entity) {
+        if (entity.hasComponents(DEPENDENCIES)) {
+            this.entities.set(entity.id, entity);
+        }
+    }
+
+    entityRemoved(entity: Entity) {
+        this.entities.delete(entity.id);
+    }
+
+    componentAdded(entity, component) {
+        if (DEPENDENCIES.indexOf(component.type) > -1) {
+            this.entities.set(entity.id, entity);
+        }
+    }
+
+    componentRemoved(entity, component) {
+        if (DEPENDENCIES.indexOf(component.type) > -1) {
+            this.entities.delete(entity.id);
+        }
+    }
+
+    activated(game: Game) {
+        this.game = game;
+        this.cacheEntities();
+    }
+
+    deactivated() {
+        this.game = null;
+        this.entities = new Map();
+    }
+
+}
+
+export = SpriteSystem;
+
+
